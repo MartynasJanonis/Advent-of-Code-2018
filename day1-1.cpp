d0d5e682f235c510f56f061909ea505974af1bdd
Martynas Janonis <martynas.janonis.17@ucl.ac.uk>
2018-12-01 23:19:21 +0000

    Reformatted code

--- day1-1.cpp
+++ day1-1.cpp
@@ -1,12 +1,13 @@
 #include <fstream>
 
-int main(){
-    std::ifstream input {"day1-1.in"};
-    std::ofstream output {"day1-1.out"};
+int main()
+{
+    std::ifstream input{"day1-1.in"};
+    std::ofstream output{"day1-1.out"};
     std::string temp;
     int sum = 0;
-    while(input >> temp){
-        sum+=std::stoi(temp);
+    while (input >> temp) {
+        sum += std::stoi(temp);
     }
     output << sum;
 }
--- day1-2.cpp
+++ day1-2.cpp
@@ -2,24 +2,30 @@
 #include <map>
 #include <vector>
 
-int main(){
-    std::ifstream input {"day1-2.in"};
-    std::ofstream output {"day1-2.out"};
+int main()
+{
+    std::ifstream input{"day1-2.in"};
+    std::ofstream output{"day1-2.out"};
     std::string temp;
-    std::vector<int>numbers;
+    std::vector<int> numbers;
     int curr_freq = 0;
-    std::map<int,int> freq;
-    while(input >> temp){
+    std::map<int, int> freq;
+
+    while (input >> temp) {
         numbers.push_back(std::stoi(temp));
     }
+
     auto it = numbers.begin();
     auto end = numbers.end();
-    while(freq[curr_freq]!=2){
-        if(it==end){it=numbers.begin();}
+
+    while (freq[curr_freq] != 2) {
+        if (it == end) {
+            it = numbers.begin();
+        }
         curr_freq += *it;
         freq[curr_freq]++;
         it++;
     }
-    output << curr_freq;
 
+    output << curr_freq;
 }
