dcfe9d2cf940cb7627e937efcb4dce8c067be1f3
Martynas Janonis <martynas.janonis.17@ucl.ac.uk>
2018-12-01 23:16:48 +0000

    Day 1 part 2 completed

---
+++ day1-2.cpp
@@ -0,0 +1,25 @@
+#include <fstream>
+#include <map>
+#include <vector>
+
+int main(){
+    std::ifstream input {"day1-2.in"};
+    std::ofstream output {"day1-2.out"};
+    std::string temp;
+    std::vector<int>numbers;
+    int curr_freq = 0;
+    std::map<int,int> freq;
+    while(input >> temp){
+        numbers.push_back(std::stoi(temp));
+    }
+    auto it = numbers.begin();
+    auto end = numbers.end();
+    while(freq[curr_freq]!=2){
+        if(it==end){it=numbers.begin();}
+        curr_freq += *it;
+        freq[curr_freq]++;
+        it++;
+    }
+    output << curr_freq;
+
+}
