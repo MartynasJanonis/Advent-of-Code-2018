fc1e795f92f4f4555c152c611344245ac46599f5
Martynas Janonis <martynas.janonis.17@ucl.ac.uk>
2018-12-01 22:50:32 +0000

    Day 1 part 1 completed

---
+++ day1-1.cpp
@@ -0,0 +1,12 @@
+#include <fstream>
+
+int main(){
+    std::ifstream input {"day1-1.in"};
+    std::ofstream output {"day1-1.out"};
+    std::string temp;
+    int sum = 0;
+    while(input >> temp){
+        sum+=std::stoi(temp);
+    }
+    output << sum;
+}
