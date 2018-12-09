8c81ead39882ac68d0cbec7223e47e8efdf15fed
Martynas Janonis <martynas.janonis.17@ucl.ac.uk>
2018-12-09 15:02:53 +0000

    Day 9 part 1 completed

---
+++ day9-1.cpp
@@ -0,0 +1,65 @@
+#include <algorithm>
+#include <fstream>
+#include <map>
+#include <vector>
+
+int main()
+{
+    std::ifstream input{"day9.in"};
+    std::ofstream output{"day9-1.out"};
+    int players, l_marble;
+    int curr_player = 1;
+    std::string skip;
+    std::map<int, int> scores;
+    std::vector<int> board;
+    input >> players >> skip >> skip >> skip >> skip >> skip >> l_marble;
+
+    // initial marble placement
+    board.push_back(0);
+    auto curr_marble = board.begin();
+
+    for (int i = 1; i <= l_marble; ++i) {
+        if (i % 23 == 0) {
+            scores[curr_player] += i;
+            for (int i = 0; i < 7; ++i) {
+                if (curr_marble != board.begin()) {
+                    --curr_marble;
+                }
+                else {
+                    curr_marble = board.end() - 1;
+                }
+            }
+            scores[curr_player] += *curr_marble;
+            curr_marble = board.erase(curr_marble);
+            if (curr_marble == board.end()) {
+                curr_marble = board.begin();
+            }
+        }
+        else {
+            // place the marble
+            if (curr_marble == board.end() - 1) {
+                curr_marble = board.begin();
+                ++curr_marble;
+                curr_marble = board.insert(curr_marble, i);
+            }
+            else {
+                curr_marble += 2;
+                curr_marble = board.insert(curr_marble, i);
+            }
+        }
+        // increment the current player
+        if (curr_player < players) {
+            ++curr_player;
+        }
+        else {
+            curr_player = 1;
+        }
+    }
+    auto high_score = std::max_element(
+        scores.begin(), scores.end(),
+        [](const std::pair<int, int>& p1, const std::pair<int, int>& p2) {
+            return p1.second < p2.second;
+        });
+
+    output << high_score->second;
+}
